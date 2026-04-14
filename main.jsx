3a6c58992fa8a5ee1356d571c7349871c4c32f23
Focus-contract <wynnementalitycoaching@gmail.com>
2026-04-15 00:04:16 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+alert("main.jsx loaded");
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
